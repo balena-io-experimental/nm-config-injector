abfea9badd272e0367f24181b4e133a8da136900
Gergely Imreh <imrehg@gmail.com>
2017-09-14 12:52:19 +0100

    wip

--- edison-ethernet.py
+++ edison-ethernet.py
@@ -4,23 +4,15 @@ import dbus
 
 s_con = dbus.Dictionary({
     'id': 'resin-wired',
-    'type': 'ethernet'})
+    'type': '802-3-ethernet'})
 
-s_con_smsc95 = dbus.Dictionary({
+s_dev = dbus.Dictionary({
     'ethernet.cloned-mac-address': 'stable',
     'match-device': 'driver:smsc95xx'})
 
-s_ip4 = dbus.Dictionary({'method': 'auto'})
-
-s_ip6 = dbus.Dictionary({
-    'addr-gen-mode':'stable-privacy',
-    'method': 'auto'})
-
 con = dbus.Dictionary({
     'connection': s_con,
-    'connection-smsc9514': s_con_smsc95,
-    'ipv4': s_ip4,
-    'ipv6': s_ip6})
+    'device': s_dev})
 
 print("Creating connection: {}".format(s_con['id']))
 
