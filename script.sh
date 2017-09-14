6718b07400eb0e1db2bb6122056ecf5c53df65e8
Gergely Imreh <imrehg@gmail.com>
2017-09-14 11:25:44 +0100

    call the script

--- script.sh
+++ script.sh
@@ -4,3 +4,5 @@ cd /tmp/
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 pip install dbus-python
+wget https://raw.githubusercontent.com/resin-io-playground/nm-config-injector/master/edison-ethernet.py
+DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket python edison-ethernet.py
