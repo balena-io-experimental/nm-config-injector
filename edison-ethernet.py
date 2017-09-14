53dbb4bc0e3f208e67188c0a33ed97d5d57e18ee
Gergely Imreh <imrehg@gmail.com>
2017-09-14 11:17:58 +0100

    initial commit

---
+++ edison-ethernet.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import dbus
+
+s_con = dbus.Dictionary({
+    'id': 'resin-wired',
+    'type': 'ethernet',
+    'ethernet.cloned-mac-address': 'stable',
+    'match-device': 'driver:smsc95xx'})
+
+s_ip4 = dbus.Dictionary({'method': 'auto'})
+
+s_ip6 = dbus.Dictionary({
+    'addr-gen-mode':'stable-privacy',
+    'method': 'auto'})
+
+con = dbus.Dictionary({
+    'connection-smsc9514': s_con,
+    'ipv4': s_ip4,
+    'ipv6': s_ip6})
+
+print("Creating connection: {}".format(s_con['id']))
+
+bus = dbus.SystemBus()
+proxy = bus.get_object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager/Settings")
+settings = dbus.Interface(proxy, "org.freedesktop.NetworkManager.Settings")
+
+settings.AddConnection(con)
+print("Connection created, reboot device to take effect.")
---
+++ script.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install python wget build-essential
+cd /tmp/
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install dbus-python
