6cb6dbc0af9680d5fb462e0dbeb4bbf716f477d7
Gergely Imreh <imrehg@gmail.com>
2017-09-15 09:55:39 +0100

    set MAC

--- edison-ethernet.py
+++ edison-ethernet.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 
 import dbus
+import sys
+
+if len(sys.argv) < 2:
+    print("Need a MAC address as an argument! Please re-run the script with that.")
+    sys.exit(1)
+
+MAC = sys.argv[1]
+print(MAC)
 
 s_con = dbus.Dictionary({
     'id': 'resin-wired',
     'type': '802-3-ethernet'})
 
-s_eth = dbus.Dictionary({'assigned-mac-address': 'random'})
+s_eth = dbus.Dictionary({'assigned-mac-address': MAC})
 
 con = dbus.Dictionary({
     'connection': s_con,
     'ethernet': s_eth})
 
-print("Creating connection: {}".format(s_con['id']))
+print("Creating connection: {} with MAC: {}".format(s_con['id'], MAC))
 
 bus = dbus.SystemBus()
 proxy = bus.get_object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager/Settings")
